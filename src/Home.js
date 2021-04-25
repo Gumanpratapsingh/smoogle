25fa02422d53121ca5aa41422a737eac920e5dcc
Gumanpratapsingh <gumanpratapsinghparmar@gmail.com>
2021-04-25 13:16:55 +0530

    done

---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import PersonSharpIcon from "@material-ui/icons/PersonSharp";
+import Search from "./Search";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_header">
+        <div className="home_headerleft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+          {/*Link */}
+        </div>
+        <div className="home_headerright">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+
+          <PersonSharpIcon />
+        </div>
+      </div>
+
+      <div className="home_body">
+        <img src="https://drive.google.com/uc?export=download&id=1KQnMX8ag3gnpJZX2vKwCvRYssS40p7mX" />
+        <div className="home_inputContainer">
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+const API_key = "AIzaSyAE-J76JEb_XmwbcHcKRSrFEtR3vuszjDg";
+
+export default API_key;
